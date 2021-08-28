8599088aabd7964450a9ac9efd6acb43c343dce2
Ben Kaan <benkaan001@gmail.com>
2021-08-27 21:18:20 -0500

    functions to generate random values

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,29 @@
 // Assignment code here
 
+// Create functions to generate different values
+
+function generateSymbols () {
+  var symbols = '!@#$%^&*(){}[]<>?';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+function generateNumbers () {
+  var numbers = '0123456789';
+  return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function generateUpperCaseLetters () {
+  var upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  return upperCaseLetters[Math.floor(Math.random() * upperCaseLetters.length)];
+}
+
+function generateLowerCaseLetters () {
+  var lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+  return lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)];
+}
+console.log(generateLowerCaseLetters());
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
