0ad94810aec292a8234b74fd7e1d713e1fe0cef9
Ben Kaan <benkaan001@gmail.com>
2021-08-29 16:51:38 -0500

    bug fix

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 // Assignment code here
 
-//Create the global elements
+//Create the global variables
 
 var symbols = '!@#$%^&*(){}[]<>?';
 var numbers = '0123456789';
@@ -12,6 +12,9 @@ var finalPassword = "";
 //Create prompts 
 
 function generatePassword() {
+  var password = "";
+  var finalPassword = "";
+
   var userInputLength = prompt ("Your password must contain minimum 8 maximum 128 characters. Enter a number.");
 
   // Confirm that user entered a valid numeric value between 8 and 128
@@ -21,12 +24,16 @@ function generatePassword() {
     isNaN(parseInt(userInputLength));
     return generatePassword();
   }
+
   // Confirm if user prefers lowercase letters
+
 
   var userInputLowerCaseLetters = confirm ("Would you like your password to include lowercase letters?");
 
   if (userInputLowerCaseLetters === true )  {
-    finalPassword = finalPassword + lowerCaseLetters;
+    finalPassword = finalPassword.concat(lowerCaseLetters);
+  } else {
+    finalPassword = finalPassword;
   }
 
   // Confirm if user prefers uppercase letters
@@ -34,7 +41,9 @@ function generatePassword() {
   var userInputUpperCaseLetters = confirm ("Would you like your password to include uppercase letters?")
   
   if (userInputUpperCaseLetters === true )  {
-    finalPassword = finalPassword + upperCaseLetters;
+    finalPassword = finalPassword.concat(upperCaseLetters);
+  } else{
+    finalPassword = finalPassword;
   }
 
   // Confirm if user prefers numbers
@@ -42,7 +51,9 @@ function generatePassword() {
   var userInputNumbers = confirm ("Would you like your password to include numeric values?");
 
   if (userInputNumbers === true ) {
-    finalPassword = finalPassword + numbers;
+    finalPassword = finalPassword.concat(numbers);
+  } else {
+    finalPassword = finalPassword;
   }
 
   //Confirm if user prefers symbols
@@ -50,7 +61,9 @@ function generatePassword() {
   var userInputSymbols = confirm ("Would you like your password to include symbols?");
 
   if (userInputSymbols === true) {
-    finalPassword = finalPassword + symbols;
+    finalPassword = finalPassword.concat(symbols);
+  } else {
+    finalPassword = finalPassword;
   }
 
   // Confirm user's choice has met at least one of the character requirements
@@ -58,64 +71,36 @@ function generatePassword() {
   if (userInputLowerCaseLetters === false && userInputUpperCaseLetters === false && userInputNumbers === false && userInputSymbols === false){
     alert ("Your password must contain at least ONE type of character. Please try it again.");
     return generatePassword();
+  };
+
+  for (var i = 0 ; i< userInputLength; i++) {
+    // debugger to inspect the for loop 
+    password = password + finalPassword[Math.floor(Math.random() * finalPassword.length)];
   }
 
+  // observe the iteration 
+  console.log(userInputLength);
+  console.log(password.length);
+  console.log(finalPassword.length);
 
-
-
-// Create functions to generate different values
-
-/*function generateSymbols () {
-  var symbols = '!@#$%^&*(){}[]<>?';
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-function generateNumbers () {
-  var numbers = '0123456789';
-  return numbers[Math.floor(Math.random() * numbers.length)];
-}
-
-function generateUpperCaseLetters () {
-  var upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  return upperCaseLetters[Math.floor(Math.random() * upperCaseLetters.length)];
-}
-
-function generateLowerCaseLetters () {
-  var lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
-  return lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)];
-} */
-
-
-
-for (var i = 0 ; i< userInputLength; i++) {
-  password = password + finalPassword[Math.floor(Math.random() * finalPassword.length)];
-}
-
-// resolve the issue in the iteration 
-console.log(userInputLength);
-console.log(password.length);
-console.log(finalPassword.length);
-
-return password;
+  return password;
 
 };
-
-
-
 
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
 
-  passwordText.value = password;
 
-}
+};
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
