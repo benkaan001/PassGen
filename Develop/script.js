be1168f81a96596d5c46111251b307390d61f495
Ben Kaan <benkaan001@gmail.com>
2021-08-28 23:46:46 -0500

    create global variables and set up prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,68 @@
 // Assignment code here
 
+//Create the global elements
+
+var symbols = '!@#$%^&*(){}[]<>?';
+var numbers = '0123456789';
+var upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+var password = "";
+var finalPassword = "";
+
+//Create prompts 
+
+function generatePassword() {
+  var userInputLength = prompt ("Your password must contain minimum 8 maximum 128 characters. Enter a number.");
+
+  // Confirm that user entered a valid numeric value between 8 and 128
+
+  while (userInputLength < 8 || userInputLength > 128 || isNaN(parseInt(userInputLength)) === true){
+    alert(" That was an invalid entry! Please enter a number value between 8 and 128!");
+    isNaN(parseInt(userInputLength));
+    return generatePassword();
+  }
+  // Confirm if user prefers lowercase letters
+
+  var userInputLowerCaseLetters = confirm ("Would you like your password to include lowercase letters?");
+
+  if (userInputLowerCaseLetters === true )  {
+    finalPassword = finalPassword + lowerCaseLetters;
+  }
+
+  // Confirm if user prefers uppercase letters
+
+  var userInputUpperCaseLetters = confirm ("Would you like your password to include uppercase letters?")
+  
+  if (userInputUpperCaseLetters === true )  {
+    finalPassword = finalPassword + upperCaseLetters;
+  }
+
+  // Confirm if user prefers numbers
+
+  var userInputNumbers = confirm ("Would you like your password to include numeric values?");
+
+  if (userInputNumbers === true ) {
+    finalPassword = finalPassword + numbers;
+  }
+
+  //Confirm if user prefers symbols
+
+  var userInputSymbols = confirm ("Would you like your password to include symbols?");
+
+  if (userInputSymbols === true) {
+    finalPassword = finalPassword + symbols;
+  }
+
+  // Confirm user's choice has met at least one of the character requirements
+
+  if (userInputLowerCaseLetters === false && userInputUpperCaseLetters === false && userInputNumbers === false && userInputSymbols === false){
+    alert ("Your password must contain at least ONE type of character. Please try it again.");
+    return generatePassword();
+  }
+
+
+
+
 // Create functions to generate different values
 
 function generateSymbols () {
@@ -21,7 +84,13 @@ function generateLowerCaseLetters () {
   var lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
   return lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)];
 }
-console.log(generateLowerCaseLetters());
+
+
+
+
+
+
+
 
 
 
